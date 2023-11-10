c706c83ffe73db6196fc46fff65d2ce7d7f25f8b
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-11-10 10:52:32 +0530

    Adding new Files

--- Docker-Session-51.sh
+++ Docker-Session-51.sh
@@ -1,3 +1,16 @@
 Docker Introduction:
 
 Vitualization --> Logical isolation of one pysical systems into multiple systems
+
+Image is Static 
+Container is Dynamic it can be runable 
+
+docker pull 
+docker create 
+docker start
+
+docker run = pull + Create + Start
+
+docker port forwarding
+
+docker 
