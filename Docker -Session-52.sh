516524dda63fcb29a321928ae1396cd8ab2551bf
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-11-11 08:11:01 +0530

    adding new files

--- Docker -Session-52.sh
+++ Docker -Session-52.sh
@@ -2,6 +2,7 @@ Topics Discussed:
 
 docker file creation:
     LABEL : Giving key value pairs to the docker file
+    
 
 docker build -t <imagename>:<image-version>
 docker inspect <image-name>
@@ -9,5 +10,4 @@ docker inspect <image-name>
 docker inspect --> to inspect the all info about docker commands
 
 For Filtering the images using labels:
-
-docker images --filter label=<key>=<value>  
+    docker images --filter label=<key>=<value>  
--- Docker.sh
+++ Docker.sh
@@ -91,7 +91,6 @@ What is the difference between docker copy and docker ADD?
 What is the difference Between ENTRYPOINT and CMD ?
 Docker Networking types
 Docker MultiStage Build
-
 Docker lables vs ENV Instructions?
 Docker ADD vs COPY Instructions?
 
